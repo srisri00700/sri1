1c199d9fbbd27cc0ddd762e21fae3006198fdbc3
srisri00700 <35294436+srisri00700@users.noreply.github.com>
2018-01-24 16:41:24 +0530

    sri3.c

---
+++ sri3.c
@@ -0,0 +1,15 @@
+include<stdio.h>
+void main()
+{
+int a,b;
+printf("\n enter the numbers");
+scanf("%d,%d,%d",&a,&b,&c);
+if((a>b)&&(a>b))
+printf("\n %d is the greatest number",a);
+else
+{
+if((b>c)&&(b>a))
+printf("\n %d is the greatest number",b);
+}
+getch();
+}
